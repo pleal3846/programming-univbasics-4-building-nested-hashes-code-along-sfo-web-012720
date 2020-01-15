d526f86218e16b8633c9b2969b91e6b57d55d300
patrick leal <pjleal3846@icloud.com>
2020-01-15 05:56:46 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,35 +1,32 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => { 
-        :name => {"Lord montague"}
-        :age {"53"}
+      :patriarch => {name: "Lord montague", age: "53"}
         #your key/value pairs here
         },
-      :matriarch => {
-        :name => {"lady montague"}
-        :age => {"54"}
+      :matriarch => { name: "Lady Montague", age:"54"}
         #your key/value pairs here
-        },
+      
       :hero => {
-        :name => {"Romeo"}
-        :age {"15"}
-        :status => {"alive"}
+        name: "Romeo", age: "15", status: "alive"}
         #your key/value pairs here
-        },
-      :hero_friends => []
+      
+      :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"}, {name: "Mercutio", age: "18", attitude: "hot-headed"}]
    }, 
    :capulet => {
-      :patriarch => {
+      :patriarch => { name: "Lord Capulet", age: "50"
         #your key/value pairs here
         },
-      :matriarch => {
+      :matriarch => {name: "Lady Capulet", age: "51",
         #your key/value pairs here
         },
-      :heroine => {
+      :heroine => {name: "Juliet", age: "15", status: "alive"
         #your key/value pairs here
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        {name: "steven", age: "30", attitude: "confused"}
+        {name: "Nurse", age: "44", attitude: "worried"}
+        ]
    }
   }
 
